024f2bd9b36e6ec6bf77874c71376810bef2f7b7
Jai-Shankar1033 <jai.shankar.9504@gmail.com>
2026-05-19 17:40:32 +0530

    fix: add geolocation and matchMedia mocks to jest setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock geolocation
+global.navigator.geolocation = {
+  getCurrentPosition: jest.fn((success) =>
+    success({ coords: { latitude: 28.6139, longitude: 77.2090 } })
+  ),
+};
+
+// Mock matchMedia
+global.matchMedia = global.matchMedia || function () {
+  return { matches: false, addListener: jest.fn(), removeListener: jest.fn() };
+};
